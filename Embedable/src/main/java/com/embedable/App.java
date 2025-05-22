13dff0fc97f4149a45bffc9575a3ad74d847bfae
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-05-23 01:03:46 +0530

    load persistence and crete entity manager for crud operation

--- Embedable/src/main/java/com/embedable/App.java
+++ Embedable/src/main/java/com/embedable/App.java
@@ -1,10 +1,17 @@
 package com.embedable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+   EntityManagerFactory emf = Persistence.createEntityManagerFactory("dev");
+   EntityManager em = emf.createEntityManager();
+   EntityTransaction et = em.getTransaction();
+   
 	}
 
 }
